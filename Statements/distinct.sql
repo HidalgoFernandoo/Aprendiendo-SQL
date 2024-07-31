f20d7aed3db4964354b497475ebf734b35acd039
Fernando Hidalgo <fervegaaa_5_@hotmail.com>
2024-07-31 19:13:13 -0300

    Command DISTINCT in MySQL

---
+++ Statements/distinct.sql
@@ -0,0 +1,5 @@
+-- DISTINCT | Lo que hace es traer los resultados distintos a lo que nosotros señalamos
+SELECT DISTINCT name FROM usuarios;
+
+-- Intentemos que nos muestre solo los campos que son distintos.
+SELECT DISTINCT fecha_inscripcion FROM global.usuarios;
