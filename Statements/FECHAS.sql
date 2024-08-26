2ccfccf6068274ad8d9ec4067e082ded9b312207
Fernando Hidalgo <fervegaaa_5_@hotmail.com>
2024-08-26 18:53:07 -0300

    Terminación Strings y Manejo de fechas

---
+++ Statements/FECHAS.sql
@@ -0,0 +1,59 @@
+-- Con la función DATE() podemos obtener la fecha de hoy. 
+-- recibe distintos nombres en cada uno de los motores.
+-- en MySQL se utiliza CURDATE(), y en Microsoft SQL Server se utiliza GETDATE()
+-- Por ejemplo la podemos utilizar en la claúsula WHERE para obtener todos lo registros de hoy.
+
+SELECT * FROM usuarios WHERE fecha_registro = DATE();
+
+-- También es posible indicar explícitamente a la función que la fecha deseada es la de hoy.
+
+SELECT * FROM usuarios WHERE fecha_registro = DATE('now');
+
+-- Se tiene una tabla llamada tareas con las siguientes columnas: "id" (identificador único)
+-- "descripcion" (descripción de la tarea) y "fecha_limite" (fecha límite para completar la tarea).
+
+-- Obtén la descripción de todas las tareas que tengan fecha_limite igual a la fecha actual.
+SELECT descripcion FROM tareas WHERE fecha_limite = DATE();
+
+-- EN SQL es posible sumar fechas para obtener fechas futuras. En SQLite lo podemos lograr pasando un segundo argumento a la función DATE. Esto suena complicado pero es mas sencillo de lo que parece:
+DATE('now', '1 day')
+-- En este ejemplo, estamos sumando 1 día a la fecha de hoy (now). Si queremos sumar más días, por ejemplo 5 días, utilizaremos DATE('now', '5 day').
+2 Semanas: DATE('now', '2 week') 3 Meses: DATE('now', '3 month')
+
+SELECT * FROM tabla where fecha > DATE('now', '2 week')
+
+-- Se tiene una tabla de tareas con los campos id, descripcion y fecha_limite. Se pide seleccionar todos los campos de las tareas que tienen como fecha límite el día de mañana.
+SELECT * FROM tareas WHERE fecha_limite = date("now", "1 day");
+
+-- RESTAR FECHAS
+-- Supongamos que tenemos una tabla llamada ganancias con las columnas "id" (identificador único), "fecha" (fecha de registro) y "monto" (ganancia del día).
+
+-- Muestra el monto correspondiente al día de ayer.
+
+
+SELECT monto FROM ganancias WHERE fecha = DATE("now", "-1 day");
+
+
+-- Extracción del año
+
+-- Nos piden mostrar toda la información de la tabla y adicionalmente agregar una columna con el año de la venta.
+
+SELECT *, strftime('%Y', fecha_venta) as año_venta FROM ventas 
+
+
+-- Dada una tabla ventas con las columnas monto y fecha_venta, crea una consulta que muestre únicamente el monto y el año de la venta. La columna que muestre el año de la venta debe llamarse año_venta
+SELECT monto, strftime("%Y", fecha_venta) as año_venta FROM ventas;
+
+-- Extracción del mes
+SELECT strftime('%m', columna) FROM tabla
+--  En lugar de usar %M, que representa los minutos (de 00 a 59), deberías usar %m, que representa el mes (de 01 a 12).
+
+-- Dada la tabla ventas previamente presentada con las columnas monto y fecha_venta, crea una consulta que muestre una tabla con el monto, el mes de la venta y el año de la venta, en ese mismo orden. La columna para el mes de la venta debe llamarse mes_venta y aquella para el año de la venta debe llamarse año_venta
+SELECT monto, strftime("%m", fecha_venta) AS mes_venta, strftime("%Y", fecha_venta) AS año_venta FROM ventas;
+
+-- Para extraer tanto el mes como el año de una fecha en una sola columna, puedes utilizar la función strftime('%m-%Y').
+-- LOS % POR DELANTE
+SELECT monto, strftime("%m-%Y", fecha_venta) AS mes_año FROM ventas;
+
+-- Dada una tabla ventas con las columnas monto y fecha_venta, selecciona toda la información de las ventas del 2015
+SELECT * FROM ventas WHERE strftime("%Y", fecha_venta) = "2015";
--- Statements/STRINGS.sql
+++ Statements/STRINGS.sql
@@ -35,3 +35,33 @@ SELECT LOWER(TRIM(email)) as email_limpios from usuarios;
 SELECT nombre, LENGTH(nombre) FROM usuarios;
 
 SELECT LENGTH(apellido) FROM usuarios;
+
+--  Si queremos obtener la cadena más corta de la columna, debemos combinar la función LENGTH() con ORDER BY y LIMIT.
+SELECT LENGTH(nombre) as largo_nombre FROM usuarios ORDER BY LENGTH(nombre) LIMIT 1 ;
+
+SELECT LENGTH(nombre) as largo_nombre FROM usuarios ORDER BY LENGTH(nombre) DESC LIMIT 1 ;
+
+-- Utiliza lo aprendido para seleccionar los 3 correos más largos de la tabla. El resultado debe mostrar dos columnas: una con los emails y otra con sus largos respectivos.
+SELECT email, LENGTH(email) FROM usuarios ORDER BY LENGTH(email) DESC LIMIT 3;
+
+-- Concatenar strings
+-- si tenemos una columna con un nombre y otra con un apellido, podemos generar una única columna con el nombre y apellido. 
+
+SELECT nombre || ' ' || apellido AS nombre_completo FROM empleados;
+
+-- Supongamos que tienes una tabla llamada productos con los campos 'producto', 'marca' y 'precio'. Selecciona una lista de todos los productos con su nombre, seguido de un guion ("-"), y su marca. Asigna el alias 'marca_producto' a la columna creada.
+SELECT producto || "-" || marca AS marca_producto FROM productos; 
+
+-- Seleccionando caracteres de un string con SUBSTR
+-- SUBSTR( string, inicio, largo )
+-- tiene 3 argumentos
+-- 1. String: el nombre de la columna o palabra que será utilizada 
+-- 2. Inicio: un integer que especifica la posicion de inicio desde la cual se extraerán caracteres al string. 
+-- 3. Largo: la cantidad de caracteres extraidos
+
+SELECT SUBSTR(nombre, 1, 1) AS primera_letra FROM productos;
+-- En este ejemplo estamos indicando que en la columna nombre, partiendo desde su primera letra, nos devuelva sólo 1 caracter que corresponderá a la primera letra de cada nombre.
+-- algunas funciones tienen distintos nombres dependiendo del motor de base de datos.
+
+-- Se tiene una tabla de usuarios con las columnas nombre y apellido. Utilizando la función SUBSTR(), selecciona 3 caracteres del apellido de María, partiendo desde el segundo caracter. Asigna el alias 'tres_caracteres_del_apellido' a la columna creada.
+SELECT SUBSTR(apellido, 2, 3) AS "tres_caracteres_del_apellido" FROM usuarios WHERE nombre = "María";
